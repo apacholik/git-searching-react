ebe74b90a7516fc5f912149581ca24697ca12a0a
apacholik <andrzej.pacholik@gmail.com>
2019-01-06 12:12:54 +0100

    Created component and add full functionality

---
+++ src/components/ResultTable.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class ResultTable extends Component {
+  render() {
+    const numberOfRows = this.props.numberOfRows || 5;
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Repo Title</th>
+            <th>Owner Stars</th>
+            <th>Create at</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.children.slice(0, numberOfRows - 1).map(row => (
+            <tr key={row.id}>
+              <td>{row.id}</td>
+              <td>{row.title}</td>
+              <td>{row.stars}</td>
+              <td>{row.createAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
