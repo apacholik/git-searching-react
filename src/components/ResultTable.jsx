845edfb64edb85b5211709328d038a337131bcb7
Andrzej Pacholik <andrzej.pacholik@gmail.com>
2019-01-09 21:29:13 +0100

    Sorting functio working

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,31 +1,74 @@
 import React from 'react';
 
-export default function ResultTable(props) {
-  const numberOfRows = props.numberOfRows || 5;
-  return (
-    <table className="table is-fullwidth table is-bordered">
-      <thead>
-        <tr>
-          <th className="has-text-right">ID</th>
-          <th className="has-text-centered">Repo Title</th>
-          <th className="has-text-centered">Owner</th>
-          <th className="has-text-centered">Stars</th>
-          <th className="has-text-centered">Create at</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.children
-          .slice(props.startItemBy, numberOfRows + props.startItemBy)
-          .map(row => (
-            <tr key={row.id}>
-              <td className="has-text-centered">{row.id}</td>
-              <td className="has-text-centered">{row.title}</td>
-              <td className="has-text-centered">{row.owner}</td>
-              <td className="has-text-centered">{row.stars}</td>
-              <td className="has-text-centered">{row.createAt}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
-  );
+export default class ResultTable extends React.Component {
+  sorting(by, type, isOrderByDESC = false) {
+    const resultModifier = isOrderByDESC ? -1 : 1;
+    // eslint-disable-next-line no-new-func
+    const convertFunction = Function(`return ${type}`)();
+    this.props.children.sort((a, b) => {
+      if (convertFunction(a[by]) === convertFunction(b[by])) return 0;
+      if (convertFunction(a[by]) > convertFunction(b[by]))
+        return 1 * resultModifier;
+      return -1 * resultModifier;
+    });
+    this.forceUpdate();
+  }
+
+  render() {
+    const numberOfRows = this.props.numberOfRows || 5;
+    return (
+      <table className="table is-fullwidth table is-bordered">
+        <thead>
+          <tr>
+            <th
+              className="has-text-right"
+              onClick={() => this.sorting('id', 'Number')}
+            >
+              ID
+            </th>
+            <th
+              className="has-text-centered"
+              onClick={() => this.sorting('title', 'String')}
+            >
+              Repo Title
+            </th>
+            <th
+              className="has-text-centered"
+              onClick={() => this.sorting('owner', 'String')}
+            >
+              Owner
+            </th>
+            <th
+              className="has-text-centered"
+              onClick={() => this.sorting('stars', 'Number')}
+            >
+              Stars
+            </th>
+            <th
+              className="has-text-centered"
+              onClick={() => this.sorting('createAt', 'String')}
+            >
+              Create at
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.children
+            .slice(
+              this.props.startItemBy,
+              numberOfRows + this.props.startItemBy
+            )
+            .map(row => (
+              <tr key={row.id}>
+                <td className="has-text-centered">{row.id}</td>
+                <td className="has-text-centered">{row.title}</td>
+                <td className="has-text-centered">{row.owner}</td>
+                <td className="has-text-centered">{row.stars}</td>
+                <td className="has-text-centered">{row.createAt}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    );
+  }
 }
