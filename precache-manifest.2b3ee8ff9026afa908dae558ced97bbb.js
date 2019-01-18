31d168c8ae315805c3ee8aee0f22ce7363f56e52
Andrzej Pacholik <apacholi@unity.pl>
2019-01-18 17:28:51 +0100

    Updates

---
+++ precache-manifest.2b3ee8ff9026afa908dae558ced97bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d010ee114516a80a497",
+    "url": "/git-searching-react/static/js/runtime~main.2d010ee1.js"
+  },
+  {
+    "revision": "d63117a39c67c8e3842c",
+    "url": "/git-searching-react/static/js/main.d63117a3.chunk.js"
+  },
+  {
+    "revision": "6d07aa5cf3cf16a09243",
+    "url": "/git-searching-react/static/js/1.6d07aa5c.chunk.js"
+  },
+  {
+    "revision": "6d07aa5cf3cf16a09243",
+    "url": "/git-searching-react/static/css/1.fa9d75e1.chunk.css"
+  },
+  {
+    "revision": "1e4df0c3d73c75422dd92abd5b52b6aa",
+    "url": "/git-searching-react/index.html"
+  }
+];
