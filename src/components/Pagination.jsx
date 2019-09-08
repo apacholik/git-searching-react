43b5f0376fe693321414a0264372088048161ff1
Andrzej Pacholik <apacholi@unity.pl>
2019-09-08 12:47:22 +0200

    Change Pagination to Function Component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Pagination extends Component {
-  render() {
-    const { pagesNumber, onClick } = this.props;
-    const actualPage = this.props.actualPage || 1;
-    if (!onClick) throw new ReferenceError('Not add props onChange');
-    const buttons = [];
-    for (let i = 1; i <= pagesNumber; i++)
-      buttons.push(
-        // eslint-disable-next-line react/button-has-type
-        <button
-          key={i}
-          className={`button is-text ${actualPage === i ? 'is-active' : ''}`}
-          onClick={() => onClick(i)}
-        >
-          {i}
-        </button>
-      );
-    return buttons;
+export default function Pagination({ pagesNumber, onClick, ...props }) {
+  const actualPage = props.actualPage || 1;
+  if (!onClick) {
+    throw new ReferenceError('Not add props onChange');
   }
+  const buttons = [];
+  for (let i = 1; i <= pagesNumber; i++)
+    buttons.push(
+      <button
+        type="button"
+        key={i}
+        className={`button is-text ${actualPage === i ? 'is-active' : ''}`}
+        onClick={() => onClick(i)}
+      >
+        {i}
+      </button>
+    );
+  return buttons;
 }
