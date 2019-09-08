abd575600fb484ad904ae4be25646a6c97f5ce59
Andrzej Pacholik <apacholi@unity.pl>
2019-09-08 13:21:59 +0200

    Change SelectorHowRows to Functional Component

--- src/components/SelectorHowRows.jsx
+++ src/components/SelectorHowRows.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SelectorHowRows extends Component {
-  render() {
-    const { maxNumberOfRows, stepBy } = this.props;
-    const options = [];
-    for (let i = stepBy; i <= maxNumberOfRows; i += stepBy) {
-      options.push(
-        <option key={i} value={i}>
-          {i}
-        </option>
-      );
-    }
-    return (
-      <div className="select is-small">
-        <select
-          onChange={e => {
-            this.props.onChange(e.target.value);
-          }}
-        >
-          {options}
-        </select>
-      </div>
+export default function SelectorHowRows({ maxNumberOfRows, stepBy, onChange }) {
+  if (!onChange) {
+    throw new ReferenceError('Not add props onChange');
+  }
+  const options = [];
+  for (let i = stepBy; i <= maxNumberOfRows; i += stepBy) {
+    options.push(
+      <option key={i} value={i}>
+        {i}
+      </option>
     );
   }
+  return (
+    <div className="select is-small">
+      <select
+        onChange={e => {
+          onChange(e.target.value);
+        }}
+      >
+        {options}
+      </select>
+    </div>
+  );
 }
